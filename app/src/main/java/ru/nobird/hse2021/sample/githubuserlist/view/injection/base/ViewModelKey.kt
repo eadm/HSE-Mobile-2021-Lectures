0b4d2a58ad791aba2efde321b96723b9927d9a3d
Ruslan Davletshin <smartmorefeed@gmail.com>
2021-08-02 11:48:00 +0300

    lecture6 / ViewModel factory

---
+++ app/src/main/java/ru/nobird/hse2021/sample/githubuserlist/view/injection/base/ViewModelKey.kt
@@ -0,0 +1,10 @@
+package ru.nobird.hse2021.sample.githubuserlist.view.injection.base
+
+import androidx.lifecycle.ViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+annotation class ViewModelKey(val value: KClass<out ViewModel>)
